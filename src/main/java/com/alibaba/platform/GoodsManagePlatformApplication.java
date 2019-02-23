77b2be1de9765eb8468596a5b2322994f3fd639c
chenweijie <chenweijie@sunlands.com>
2019-02-24 01:21:59 +0800

    init

---
+++ src/main/java/com/alibaba/platform/GoodsManagePlatformApplication.java
@@ -0,0 +1,18 @@
+package com.alibaba.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author chenweijie
+ * @date
+ */
+@SpringBootApplication
+public class GoodsManagePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsManagePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alibaba/platform/GoodsManagePlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alibaba.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodsManagePlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
